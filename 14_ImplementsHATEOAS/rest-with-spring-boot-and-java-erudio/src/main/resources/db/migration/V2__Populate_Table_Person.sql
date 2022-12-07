b684deb5edacd1ee6a0feae645ea88ab7788c544
Nicolas Ramires <Nicolas96_ramires@hotmail.com>
2022-12-07 12:15:50 -0300

    FirstStepsInJavaWithSpringBoot

---
+++ 14_ImplementsHATEOAS/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vince'),
+	(3, 'Porbandar - India', 'Indira', 'Male', 'Gandhi'),
+	(4, 'Kentucky - US', 'Ali', 'Male', 'Muhammad');
